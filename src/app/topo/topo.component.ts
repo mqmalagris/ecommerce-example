cc4995fa54efecfe0ff37c301c30dc3a39a0610f
mqmalagris <malagrismatheus@gmail.com>
2020-06-04 18:32:04 -0300

    pesquisa

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.services';
 import { Oferta } from '../shared/oferta.model';
-import { Observable } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
+import { switchMap, debounceTime, distinctUntilChanged, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-topo',
@@ -12,19 +13,39 @@ import { Observable } from 'rxjs';
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>
+  public ofertas2: Oferta[]
+  private subjectPesquisa: Subject<string> = new Subject<string>()
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit(): void {
+    this.ofertas = this.subjectPesquisa
+      .pipe(
+        debounceTime(1000),
+        distinctUntilChanged(),
+        switchMap((termo:string)=>{
+          if(termo.trim() === ''){
+            // retornar observable de array de ofertas vazio
+            return of<Oferta[]>([])
+          }
+          return this.ofertasService.pesquisaOfertas(termo)
+        }),
+        catchError((err: any, observable : Observable<Oferta[]>) => {
+          console.log(err)
+          return of<Oferta[]>([])
+        })
+      )
+
+    this.ofertas.subscribe((ofertas : Oferta[]) => {
+      console.log(ofertas)
+      this.ofertas2 = ofertas
+    }
+    )
   }
 
   public pesquisa(termoDaBusca: string): void {
-    this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca)
+    console.log("keyup caracter:", termoDaBusca);
 
-    this.ofertas.subscribe(
-      (ofertas: Oferta[]) => console.log(ofertas),
-      (erro: any) => console.log('Erro status: ', erro.status),
-      () => null
-    )
+    this.subjectPesquisa.next(termoDaBusca)
   }
 }
